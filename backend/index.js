e9d818c03f9fad2c0824c0845d2d356712879ab5
mayada-dev218 <mayadamahmoud2580@gmail.com>
2026-08-07 23:12:56 +0300

    Update index.js with direct mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/database');
+const mongoose = require('mongoose');
 
 const authRoutes = require('./routes/authRoutes');
 const patientRoutes = require('./routes/patientRoutes');
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
 
 app.use(express.json());
+
+// الاتصال بقاعدة البيانات
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log('DB Connection Error:', err));
 
 // مسارات المصادقة والمريض
 app.use('/api/auth', authRoutes);
