94e5ff927166961e5b957308259e627e4783c149
Mohamed Eid <meid29013@gmail.com>
2026-08-07 18:51:41 +0300

    add patient routes

--- backend/routes/PatientRoutes.js
+++ backend/routes/PatientRoutes.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getPatientProfile, updatePatientProfile } = require('../controllers/PatientController');
-const { protect } = require('../middlewares/auth');
+const {
+  getPatientProfile,
+  createPatientProfile,
+  updatePatientProfile,
+} = require("../controllers/PatientController");
+const { protect } = require("../middlewares/auth");
 
 // مسار جلب بيانات المريض
-router.get('/profile', protect, getPatientProfile);
+router.get("/profile", protect, getPatientProfile);
+
+// مسار إنشاء بيانات المريض لأول مرة
+router.post("/profile", protect, createPatientProfile);
 
 // مسار تحديث بيانات المريض
-router.put('/profile', protect, updatePatientProfile);
+router.put("/profile", protect, updatePatientProfile);
 
 module.exports = router;
