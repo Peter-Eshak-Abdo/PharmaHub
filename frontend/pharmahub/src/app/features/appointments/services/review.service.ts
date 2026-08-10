dc76e98e6e91c493021880b75a24e80d1682e6bc
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-08-10 12:41:13 +0300

    Create services of featuer

--- frontend/pharmahub/src/app/features/appointments/services/review.service.ts
+++ frontend/pharmahub/src/app/features/appointments/services/review.service.ts
@@ -1,9 +1,21 @@
+// features/appointments/services/review.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReviewService {
+  private apiUrl = '/api/reviews';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addReview(appointmentId: string, reviewData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/${appointmentId}`, reviewData);
+  }
+
+  getDoctorReviews(doctorId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/doctor/${doctorId}`);
+  }
 }
