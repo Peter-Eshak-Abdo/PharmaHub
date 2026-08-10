63d90e9b95e92d5b5e96a864bc3ebf177a080b88
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-08-10 12:40:21 +0300

    Create appoinment route & module

--- frontend/pharmahub/src/app/features/appointments/appointments.module.ts
+++ frontend/pharmahub/src/app/features/appointments/appointments.module.ts
@@ -1,23 +1,26 @@
+// features/appointments/appointments.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppointmentsRoutingModule } from './appointments-routing.module';
 import { BookingFormComponent } from './booking-form/booking-form.component';
 import { PatientAppointmentsComponent } from './patient-appointments/patient-appointments.component';
 import { DoctorAppointmentsComponent } from './doctor-appointments/doctor-appointments.component';
 import { ReviewFormComponent } from './review-form/review-form.component';
 
-
 @NgModule({
   declarations: [
     BookingFormComponent,
     PatientAppointmentsComponent,
     DoctorAppointmentsComponent,
-    ReviewFormComponent
+    ReviewFormComponent,
   ],
   imports: [
     CommonModule,
-    AppointmentsRoutingModule
-  ]
+    ReactiveFormsModule,
+    FormsModule,
+    AppointmentsRoutingModule,
+  ],
+  exports: [ReviewFormComponent],
 })
-export class AppointmentsModule { }
+export class AppointmentsModule {}
