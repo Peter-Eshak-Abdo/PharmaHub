eae89335a6f8834c2f8b0a8e4c8ab6ee6cdfd8a7
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-08-10 12:47:19 +0300

    create Review form files

--- frontend/pharmahub/src/app/features/appointments/review-form/review-form.component.ts
+++ frontend/pharmahub/src/app/features/appointments/review-form/review-form.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+// features/appointments/review-form/review-form.component.ts
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ReviewService } from '../services/review.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-review-form',
   templateUrl: './review-form.component.html',
-  styleUrls: ['./review-form.component.css']
+  styleUrls: ['./review-form.component.css'],
 })
 export class ReviewFormComponent {
+  @Input() appointmentId!: string;
+  @Output() reviewSubmitted = new EventEmitter<void>();
+  reviewForm: FormGroup;
+  showForm = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private reviewService: ReviewService,
+  ) {
+    this.reviewForm = this.fb.group({
+      rating: [5, [Validators.required, Validators.min(1), Validators.max(5)]],
+      comment: [''],
+    });
+  }
+
+  submitReview(): void {
+    if (this.reviewForm.valid) {
+      this.reviewService
+        .addReview(this.appointmentId, this.reviewForm.value)
+        .subscribe(() => {
+          this.showForm = false;
+          this.reviewSubmitted.emit();
+        });
+    }
+  }
 }
