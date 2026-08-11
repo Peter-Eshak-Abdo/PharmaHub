2aa167e5f9f084daf00175d42837a6ae84d7f45c
Mohamed Eid <meid29013@gmail.com>
2026-08-11 14:00:03 +0300

    feat(profiles): add patient and doctor services for API integration

---
+++ frontend/src/app/features/profiles/services/patient.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+  private apiUrl = '/api/patients';
+
+  constructor(private http: HttpClient) {}
+
+  getPatientProfile(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/profile`);
+  }
+
+  updatePatientProfile(data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/profile`, data);
+  }
+}
