e0ffd4762ff47b5127a3484956ce51fb7720b711
mayada-dev218 <mayadamahmoud2580@gmail.com>
2026-08-11 09:07:03 +0300

    update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,30 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { RoleGuard } from './core/guards/role.guard';
-import { DummyAdminComponent, DummyDoctorComponent, DummyPatientComponent } from './test-c';
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) //[cite: 1]
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) 
   },
   { 
     path: 'admin-dashboard', 
-    component: DummyAdminComponent, 
-    canActivate: [AuthGuard, RoleGuard], //[cite: 1]
-    data: { expectedRole: 'admin' } //[cite: 1]
+    canActivate: [AuthGuard, RoleGuard], 
+    data: { expectedRole: 'admin' } 
   },
   { 
     path: 'doctor-dashboard', 
-    component: DummyDoctorComponent, 
-    canActivate: [AuthGuard, RoleGuard], //[cite: 1]
-    data: { expectedRole: 'doctor' } //[cite: 1]
+
+    canActivate: [AuthGuard, RoleGuard], 
+    data: { expectedRole: 'doctor' } 
   },
   { 
     path: 'appointments', 
-    component: DummyPatientComponent, 
-    canActivate: [AuthGuard, RoleGuard], //[cite: 1]
-    data: { expectedRole: 'patient' } //[cite: 1]
+
+    canActivate: [AuthGuard, RoleGuard], 
+    data: { expectedRole: 'patient' } 
   },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: '**', redirectTo: 'auth/login' }
