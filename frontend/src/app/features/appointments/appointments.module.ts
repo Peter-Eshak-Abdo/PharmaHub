d6db6b4aa7634309e62583ea5583ef08386d6f12
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-08-12 14:00:34 +0300

    Finally Fix all 11 error

--- frontend/src/app/features/appointments/appointments.module.ts
+++ frontend/src/app/features/appointments/appointments.module.ts
@@ -5,16 +5,16 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppointmentsRoutingModule } from './appointments-routing.module';
-// import { BookingFormComponent } from './booking-form/booking-form.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
 import { PatientAppointmentsComponent } from './patient-appointments/patient-appointments.component';
-// import { DoctorAppointmentsComponent } from './doctor-appointments/doctor-appointments.component';
+import { DoctorAppointmentsComponent } from './doctor-appointments/doctor-appointments.component';
 import { ReviewFormComponent } from './review-form/review-form.component';
 
 @NgModule({
   declarations: [
-    // BookingFormComponent,
+    BookingFormComponent,
     PatientAppointmentsComponent,
-    // DoctorAppointmentsComponent,
+    DoctorAppointmentsComponent,
     ReviewFormComponent,
   ],
   imports: [
