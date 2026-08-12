5894643c8d0e9b86463bc5f7105327e59d87a470
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-08-12 23:55:44 +0300

    move vercel.json file tin backend

--- backend/index.js
+++ backend/index.js
@@ -45,8 +45,10 @@ app.use("/api/patients", medicalHistoryRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = app;
