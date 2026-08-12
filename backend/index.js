78d09c8af34150dee03d35121614c8b9a3eaba42
Minamagdy272 <2200675@eng.asu.edu.eg>
2026-08-12 13:17:32 +0300

    reupdating the port problem

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ const diagnosisRoutes = require("./routes/Diagnosisroutes");
 const medicationRoutes = require("./routes/Medicationroutes");
 const prescriptionRoutes = require("./routes/Prescriptionroutes");
 const reviewRoutes = require("./routes/ReviewRoutes");
-const medicalHistoryRoutes = require("./routes/medicalHistoryRoutes");
 
 dotenv.config();
 
@@ -33,7 +32,6 @@ connectDB();
 // مسارات المصادقة والمريض
 app.use("/api/auth", authRoutes);
 app.use("/api/patient", patientRoutes);
-app.use("/api/patient", medicalHistoryRoutes);
 app.use("/api/doctor", doctorRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/availability", availabilityRoutes);
@@ -43,8 +41,7 @@ app.use("/api/medications", medicationRoutes);
 app.use("/api/prescriptions", prescriptionRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
