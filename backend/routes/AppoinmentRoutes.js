814b41671e51912318d39e2b17e422e9d8c0d178
Minamagdy272 <2200675@eng.asu.edu.eg>
2026-08-12 13:10:55 +0300

    update backend routes files

--- backend/routes/AppoinmentRoutes.js
+++ backend/routes/AppoinmentRoutes.js
@@ -10,8 +10,11 @@ const {
 const { protect, checkRole } = require("../middlewares/auth");
 
 router.post("/", protect, checkRole(["patient"]), createAppointment);// إنشاء حجز جديد
+router.get("/patient", protect, getPatientAppointments); // جلب حجوزات المريض المسجل
 router.get("/patient/:patientId", getPatientAppointments);// جلب حجوزات مريض محدد
+router.get("/doctor", protect, getDoctorAppointments); // جلب حجوزات الطبيب المسجل
 router.get("/doctor/:doctorId", getDoctorAppointments);// جلب حجوزات طبيب محدد
 router.patch("/:id/status", updateAppointmentStatus);// تحديث حالة الحجز (مكتمل / ملغي / ...)
 
 module.exports = router;
+
