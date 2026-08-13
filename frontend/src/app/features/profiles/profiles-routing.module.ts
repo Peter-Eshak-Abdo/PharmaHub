0baffb89a0db9ef90ad43adc1734776a796f2a1c
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-08-13 09:44:57 +0300

    add routing to doctor profile

--- frontend/src/app/features/profiles/profiles-routing.module.ts
+++ frontend/src/app/features/profiles/profiles-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PatientProfileComponent } from './patient-profile/patient-profile.component';
 import { DoctorListComponent } from './doctor-list/doctor-list.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
 import { DoctorDetailComponent } from './doctor-detail/doctor-detail.component';
 
 const routes: Routes = [
   { path: 'patient-profile', component: PatientProfileComponent },
   { path: 'doctor-list', component: DoctorListComponent },
-  { path: 'doctor-detail/:id', component: DoctorDetailComponent }
+  { path: 'doctor-profile', component: DoctorProfileComponent },
+  { path: 'doctor-detail/:id', component: DoctorDetailComponent },
 ];
 
 @NgModule({
